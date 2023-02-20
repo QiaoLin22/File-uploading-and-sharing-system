9672aaab15fa19c4500bc25e418e3de392153b45
Qiao Lin <qiaolin@andrew.cmu.edu>
2023-02-19 18:28:11 -0800

    added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/QiaoLin22/Golang-production-1/internal/transport/http"
+)
 
 // App, the struct which contains things like pointers to db connections
 type App struct{}
 
 func (app *App) Run() error {
 	fmt.Println("Setting up App")
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to setup server")
+		return err
+	}
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/QiaoLin22/Golang-production-1
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0
