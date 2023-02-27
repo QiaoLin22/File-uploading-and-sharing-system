fdd59f88d7ca4805cd5e7fcca5e1693880a9c164
Qiao Lin <qiaolin@andrew.cmu.edu>
2023-02-26 16:05:17 -0800

    create db layer

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/QiaoLin22/Golang-production-1/internal/comment"
+	"github.com/QiaoLin22/Golang-production-1/internal/db"
+)
+
+// initialize and start up of the go application
+func Run() error {
+	fmt.Println("starting up application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to database")
+		return err
+	}
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate database")
+		return err
+	}
+	fmt.Println("Connected to and pinged database")
+
+	cmtService := comment.NewService(db)
+	fmt.Println(cmtService.GetComment(
+		context.Background(),
+		"71c5d074-b6cf-11ec-b909-0242ac120002",
+	))
+	return nil
+}
 
 func main() {
 	fmt.Println("Hello")
+	if err := Run(); err != nil {
+		fmt.Println(err)
+	}
 }
---
+++ internal/comment/comment.go
@@ -0,0 +1,58 @@
+package comment
+
+import (
+	"context"
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrFetchingComment = errors.New("failed to fetch comment by id")
+	ErrNotImplemented  = errors.New("not implemented")
+)
+
+type Comment struct {
+	ID     string `json:"id"`
+	Slug   string `json:"slug"`
+	Body   string `json:"body"`
+	Author string `json:"author"`
+}
+
+// Store - this interface defines all methods that our service needs
+type Store interface {
+	GetComment(context.Context, string) (Comment, error)
+}
+
+// Service - the struct on which our logic build on top of
+type Service struct {
+	Store Store
+}
+
+// NewService - return a pointer to a new Service
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	fmt.Println("retrieving a comment")
+	cmt, err := s.Store.GetComment(ctx, id)
+	if err != nil {
+		fmt.Println(err)
+		return Comment{}, ErrFetchingComment
+	}
+	return cmt, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, cmt Comment) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
+}
